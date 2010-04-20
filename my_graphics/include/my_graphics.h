452d375a432f860f09fe3539610a1f815bbf6ce1
ant01n3 <ant01n3@5f1e6f3c-8dd1-45c4-9ebf-e2a42b03f746>
2010-04-20 13:12:04 +0000

    git-svn-id: http://svn2.assembla.com/svn/ant01n3/trunk@31 5f1e6f3c-8dd1-45c4-9ebf-e2a42b03f746

---
+++ my_graphics/include/my_graphics.h
@@ -0,0 +1,37 @@
+/*
+** my_graphics.h for my_graphics in /u/all/lucian_b/cu/work/my_graphics
+** 
+** Made by antoine luciani
+** Login   <lucian_b@epitech.net>
+** 
+** Started on  Tue Apr 20 14:58:23 2010 antoine luciani
+** Last update Tue Apr 20 15:06:24 2010 antoine luciani
+*/
+
+#ifndef _MY_GRAPHICS_H_
+# define _MY_GRAPHICS_H_
+
+#include "mgr_device.h"
+#include "mgr_window.h"
+
+typedef enum	e_mgr_error
+  {
+    MGR_ERROR_NONE,
+    MGR_ERROR_DEVICE
+  }		t_mgr_error;
+
+/*
+** Initializes my_graphics.
+**
+** @return a t_mgr_error.
+*/
+t_mgr_error	mgr_init();
+
+/*
+** Clean up all stuff before exiting the application.
+** Don't forget to call this function when you have finished with
+** my_graphics.
+*/
+void		mgr_quit();
+
+#endif /* !_MY_GRAPHICS_H_ */
