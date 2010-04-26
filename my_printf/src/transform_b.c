c421db195ff779835bdd39f61f49be494163f1d9
ant01n3 <ant01n3@5f1e6f3c-8dd1-45c4-9ebf-e2a42b03f746>
2010-04-26 11:57:14 +0000

    Added the latest version of libmy and a missing source file (transform_b.c).
    git-svn-id: http://svn2.assembla.com/svn/ant01n3/trunk@49 5f1e6f3c-8dd1-45c4-9ebf-e2a42b03f746

---
+++ my_printf/src/transform_b.c
@@ -0,0 +1,24 @@
+/*
+** transform_b.c for my_printf in /u/all/lucian_b/cu/work/projects/my_printf
+** 
+** Made by antoine luciani
+** Login   <lucian_b@epitech.net>
+** 
+** Started on  Mon Apr 26 11:39:44 2010 antoine luciani
+** Last update Mon Apr 26 11:58:18 2010 antoine luciani
+*/
+
+#include "request.h"
+#include "my.h"
+
+#include <stdarg.h>
+
+#define BINARY_BASE "01"
+
+void		transform_b(t_request *request, va_list *ap)
+{
+  unsigned int	nb;
+
+  nb = va_arg(ap);
+  my_putnbr_base(nv, BINARY_BASE);
+}
