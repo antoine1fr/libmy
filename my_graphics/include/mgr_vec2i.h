c7c6dc2f941bdf4dd9a6334a8514ed712be40843
ant01n3 <ant01n3@5f1e6f3c-8dd1-45c4-9ebf-e2a42b03f746>
2010-05-06 09:38:17 +0000

    Added the type t_mgr_vec2i in my_graphics for support of  2-integer-dimension vectors.
    git-svn-id: http://svn2.assembla.com/svn/ant01n3/trunk@107 5f1e6f3c-8dd1-45c4-9ebf-e2a42b03f746

---
+++ my_graphics/include/mgr_vec2i.h
@@ -0,0 +1,20 @@
+/*
+** mgr_vec2i.h for my_graphics in /u/all/lucian_b/cu/work/my_graphics
+** 
+** Made by antoine luciani
+** Login   <lucian_b@epitech.net>
+** 
+** Started on  Thu May  6 11:33:06 2010 antoine luciani
+** Last update Thu May  6 11:34:28 2010 antoine luciani
+*/
+
+#ifndef _MGR_VEC2I_H_
+# define _MGR_VEC2I_H_
+
+typedef struct	s_mgr_vec2i
+{
+  int		x;
+  int		y;
+}		t_mgr_vec2i;
+
+#endif /* _MGR_VEC2I_H_ */
