cf5525a3efa56671582458c88695452f9669964d
ant01n3 <ant01n3@5f1e6f3c-8dd1-45c4-9ebf-e2a42b03f746>
2010-05-12 21:51:19 +0000

    git-svn-id: http://svn2.assembla.com/svn/ant01n3/trunk@134 5f1e6f3c-8dd1-45c4-9ebf-e2a42b03f746

--- fdf/src/main.c
+++ fdf/src/main.c
@@ -5,12 +5,14 @@
 ** Login   <lucian_b@epitech.net>
 ** 
 ** Started on  Tue May 11 17:31:14 2010 antoine luciani
-** Last update Wed May 12 21:05:50 2010 antoine luciani
+** Last update Wed May 12 23:48:01 2010 antoine luciani
 */
 
 #include <stdlib.h>
 #include <fcntl.h>
 #include <stdio.h>
+
+#include <math.h>
 
 #include "map.h"
 #include "list.h"
@@ -19,35 +21,10 @@
 #include "vector4f.h"
 #include "my_graphics.h"
 
-#define ISOMETRIC_K1 1.0f
-#define ISOMETRIC_K2 1.0f
-
-static void	print_token_info(t_token *token_ptr)
-{
-  if (!token_ptr)
-    return;
-  my_putstr("type\t: ");
-  my_put_nbr(token_ptr->type);
-  my_putstr("\nvalue\t: ");
-  my_put_nbr(token_ptr->value);
-  my_putchar('\n');
-}
-
-static int	test_tokenize_file(const char *file_name)
-{
-  t_list	list;
-  int		file_desc;
-
-  file_desc = open(file_name, O_RDONLY);
-  if (file_desc == -1)
-    return (1);
-  list_init(&list);
-  map_tokenize_file(file_desc, &list);
-  printf("list's size : %d\n", list.node_count);
-  list_print_data(&list, print_token_info);
-  list_clean(&list);
-  return (0);
-}
+#define WND_WIDTH 400
+#define WND_HEIGHT 400
+#define K1 0.75f
+#define K2 0.75f
 
 void		print_map_from_tokens(t_list *list_ptr)
 {
@@ -85,24 +62,62 @@ void		print_map(t_map *map_ptr)
     }
 }
 
+/*
+void	draw_circle(t_mgr_image *img)
+{
+  t_mgr_vec2i	centre;
+  t_mgr_vec2i	radius;
+  int		i;
+
+  i = 0;
+  centre.x = 0;
+  centre.y = 0;
+  while (i < 360)
+    {
+      radius.x = cos(i * 3.14157 / 180) * 50;
+      radius.y = sin(i * 3.14157 / 180) * 50;
+      mgr_draw_line(&centre, &radius, img, 0xffffffff);
+      i += 1;
+    }
+}
+*/
+
 int		main()
 {
+  /*
+    t_matrix4f	mat = {
+    {1.f, 0.f, 0.f, WND_WIDTH / 2.f},
+    {0.f, 1.f, 0.f, WND_HEIGHT / 2.f},
+    {0.f, 0.f, 1.f, 0.f},
+    {0.f, 0.f, 0.f, 1.f}
+    };
+  */
   t_matrix4f	mat = {
-    {ISOMETRIC_K1, -ISOMETRIC_K2, 0.f, 0.f},
-    {ISOMETRIC_K1 / 2.f, ISOMETRIC_K2 / 2.f, 1.f, 0.f},
+    {K1, -K2, 0.f, 0.f},
+    {K1 / 2.f, K2 / 2.f, 1.f, 0.f},
     {0.f, 0.f, 0.f, 0.f},
     {0.f, 0.f, 0.f, 1.f}
   };
+  /*
+    t_matrix4f	mat = {
+    {1.f, 0.f, 1.f, WND_WIDTH / 2.f},
+    {0.f, 1.f, 0.5f, WND_HEIGHT / 2.f},
+    {0.f, 0.f, 0.f, 0.f},
+    {0.f, 0.f, 0.f, 1.f}
+    };
+  */
   t_vector4f	quad[4] = {
-    {0.f, 0.f, 0.f, 1.f},
-    {10.f, 0.f, 0.f, 1.f},
-    {10.f, 10.f, 0.f, 1.f},
-    {0.f, 10.f, 0.f, 1.f}
+    {0.f, 0.f, 100.f, 1.f},
+    {100.f, 0.f, 100.f, 1.f},
+    {100.f, 100.f, 100.f, 1.f},
+    {0.f, 100.f, 0.f, 1.f}
   };
   t_vector4f	quad_proj[4];
   int		i;
   t_mgr_window	*wnd_ptr;
   t_mgr_image	*back_buffer;
+  t_mgr_vec2i	beg;
+  t_mgr_vec2i	end;
 
   mgr_init();
   wnd_ptr = mgr_create_window("test - proj", 400, 400);
@@ -110,10 +125,42 @@ int		main()
   i = 0;
   while (i < 4)
     {
-      mat4f_mult_vec4f(mat, &quad[i], &quad_proj[i]);
+      /*
+	mat4f_mult_vec4f(mat, &quad[i], &quad_proj[i]);
+      */
+      quad_proj[i].x = K1 * quad[i].x - K2 * quad[i].y;
+      quad_proj[i].y = quad[i].z + K1 * quad[i].x / 2.f + K2 * quad[i].y / 2.f;
+      quad_proj[i].z = 0;
+      quad_proj[i].w = 1;
       i += 1;
     }
-  mgr_destroy(wnd_ptr);
+  /*
+    beg.x = quad_proj[0].x;
+    beg.y = quad_proj[0].y;
+    end.x = quad_proj[1].x;
+    end.y = quad_proj[1].y;
+    mgr_draw_line(&beg, &end, back_buffer, 0xffffffff);
+    beg.x = quad_proj[2].x;
+    beg.y = quad_proj[2].y;
+    end.x = quad_proj[1].x;
+    end.y = quad_proj[1].y;
+    mgr_draw_line(&beg, &end, back_buffer, 0xffffffff);
+  */
+  beg.x = quad_proj[2].x;
+  beg.y = quad_proj[2].y;
+  end.x = quad_proj[3].x;
+  end.y = quad_proj[3].y;
+  mgr_draw_line(&beg, &end, back_buffer, 0xffffffff);
+  /*
+    beg.x = quad_proj[3].x;
+    beg.y = quad_proj[3].y;
+    end.x = quad_proj[0].x;
+    end.y = quad_proj[0].y;
+    mgr_draw_line(&beg, &end, back_buffer, 0xffffffff);
+  */
+  while (1)
+    mgr_wnd_swap_chain(wnd_ptr);
+  mgr_destroy_window(wnd_ptr);
   mgr_quit();
   return (EXIT_SUCCESS);
 }
