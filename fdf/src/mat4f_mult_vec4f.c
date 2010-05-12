5b5ca9621e52b37c10140dc2d53a04fa06206372
ant01n3 <ant01n3@5f1e6f3c-8dd1-45c4-9ebf-e2a42b03f746>
2010-05-12 17:28:56 +0000

    In FDF : very basic implementation of vector4 and matrix 4x4.
    git-svn-id: http://svn2.assembla.com/svn/ant01n3/trunk@130 5f1e6f3c-8dd1-45c4-9ebf-e2a42b03f746

---
+++ fdf/src/mat4f_mult_vec4f.c
@@ -0,0 +1,28 @@
+/*
+** mat4f_mult_vec4f.c for fdf in /u/all/lucian_b/cu/work/projects/fdf
+** 
+** Made by antoine luciani
+** Login   <lucian_b@epitech.net>
+** 
+** Started on  Wed May 12 19:04:02 2010 antoine luciani
+** Last update Wed May 12 19:27:08 2010 antoine luciani
+*/
+
+#include "matrix4f.h"
+#include "vector4f.h"
+
+void		mat4f_mult_vec4f(t_matrix4f mat,
+				 struct s_vector4f *v,
+				 struct s_vector4f *result)
+{
+  if (!result || !v)
+    return;
+  result->x = mat[0][0] * v->x + mat[0][1] * v->y;
+  result->x += mat[0][2] * v->z + mat[0][3] * v->z;
+  result->y = mat[1][0] * v->x + mat[1][1] * v->y;
+  result->y += mat[1][2] * v->z + mat[1][3] * v->z;
+  result->z = mat[2][0] * v->x + mat[2][1] * v->y;
+  result->z += mat[2][2] * v->z + mat[2][3] * v->z;
+  result->w = mat[3][0] * v->x + mat[3][1] * v->y;
+  result->w += mat[3][2] * v->z + mat[3][3] * v->z;
+}
