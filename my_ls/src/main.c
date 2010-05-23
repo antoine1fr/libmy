f9c3b5ea4de675e0908f4be84cad7b4a60751787
ant01n3 <ant01n3@5f1e6f3c-8dd1-45c4-9ebf-e2a42b03f746>
2010-05-23 21:14:53 +0000

    git-svn-id: http://svn2.assembla.com/svn/ant01n3/trunk@143 5f1e6f3c-8dd1-45c4-9ebf-e2a42b03f746

--- my_ls/src/main.c
+++ my_ls/src/main.c
@@ -10,46 +10,62 @@
 
 #include <stdlib.h>
 #include <sys/types.h>
+#include <sys/stat.h>
 #include <dirent.h>
+#include <unistd.h>
 #include "my.h"
 
 #define ERROR_NO_ERROR 0
 #define ERROR_READ_DIR 1
 
-typedef unsigned char t_error;
+#define MY_FALSE 0
+#define MY_TRUE 1
 
-static void	print_error(t_error error)
+typedef char	t_error;
+typedef char	t_bool;
+
+void	print_error(t_error error)
 {
-  if (ERROR_READ_DIR)
+  if (error == ERROR_READ_DIR)
     my_puterr("ERROR : failed to read the directory.\n");
 }
 
-t_error		list_dir_content(const char *path)
+t_error		list_dir_content(const char *path, t_bool print_hidden)
 {
   DIR		*dir_ptr;
-  dirent	*dirent_ptr;
+  struct dirent	*dirent_ptr;
 
-  dir_ptr = readdir(path);
+  dir_ptr = opendir(path);
   if (!dir_ptr)
     return (ERROR_READ_DIR);
   while ((dirent_ptr = readdir(dir_ptr)) != 0)
     {
-      my_putstr(dirent_ptr->d_name);
-      my_putchar('\n');
+      if (dirent_ptr->d_name[0] != '.' || print_hidden == MY_TRUE)
+	{
+	  my_putstr(dirent_ptr->d_name);
+	  my_putchar('\n');
+	}
     }
+  closedir(dir_ptr);
   return (ERROR_NO_ERROR);
+}
+
+void		test_stat(const char *path)
+{
+  struct stat	buf;
+
+  stat(path, &buf);
+  my_put_nbr(buf.st_mtime);
+  my_putchar('\n');
 }
 
 int		main(int argc, char **argv)
 {
-  t_error	error_code;
-
   if (argc != 2)
     {
-      my_puterr("Usage : ./my_ls path\m");
+      my_puterr("Usage : ./my_ls path\n");
       return (EXIT_FAILURE);
     }
-  error_code = list_dir_content(argv[1]);
-  print_error(error_code);
+  test_stat(argv[1]);
   return (EXIT_SUCCESS);
 }
