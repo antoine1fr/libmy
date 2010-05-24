66a908742873033eecd344e2a63856e59bbcbfbd
ant01n3 <ant01n3@5f1e6f3c-8dd1-45c4-9ebf-e2a42b03f746>
2010-05-24 21:02:00 +0000

    git-svn-id: http://svn2.assembla.com/svn/ant01n3/trunk@150 5f1e6f3c-8dd1-45c4-9ebf-e2a42b03f746

--- my_ls/src/main.c
+++ my_ls/src/main.c
@@ -15,6 +15,7 @@
 #include <unistd.h>
 #include <dirent.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 #include "btree.h"
 #include "list.h"
@@ -35,15 +36,34 @@ void		clean_element(void *ptr)
   free(elt_ptr);
 }
 
+void		simple_dir_print(t_btree_node *node_ptr)
+{
+  t_mls_element	*elt_ptr;
+
+  if (!node_ptr)
+    return;
+  simple_dir_print(node_ptr->left_ptr);
+  elt_ptr = (t_mls_element *)node_ptr->data;
+  printf("%s\n", elt_ptr->dirent_ptr->d_name);
+  simple_dir_print(node_ptr->right_ptr);
+}
+
 int		main(int argc, char **argv)
 {
   t_btree	elt_tree;
   t_list	dir_list;
+  t_error	error_code;
 
   if (argc != 2)
-    return (EXIT_FAILURE);
+    {
+      my_puterr("ERROR : missing argument(s)!\n");
+      return (EXIT_FAILURE);
+    }
   btree_init(&elt_tree, comp_elements, clean_element);
   list_init(&dir_list);
-  mls_read_dir(argv[1], 0, &elt_tree, &dir_list);
+  error_code = mls_read_dir(argv[1], 0, &elt_tree, &dir_list);
+  simple_dir_print(elt_tree.root_ptr);
+  btree_clean(&elt_tree);
+  list_clean(&dir_list);
   return (EXIT_SUCCESS);
 }
