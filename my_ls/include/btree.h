dcd974616a25fdfecc271e36415271c65bd29bee
ant01n3 <ant01n3@5f1e6f3c-8dd1-45c4-9ebf-e2a42b03f746>
2010-05-24 08:41:43 +0000

    In my_ls : began the implementation of binary trees.
    git-svn-id: http://svn2.assembla.com/svn/ant01n3/trunk@144 5f1e6f3c-8dd1-45c4-9ebf-e2a42b03f746

---
+++ my_ls/include/btree.h
@@ -0,0 +1,22 @@
+/*
+** btree.h for my_ls in /u/all/lucian_b/cu/work/projects/my_ls
+** 
+** Made by antoine luciani
+** Login   <lucian_b@epitech.net>
+** 
+** Started on  Mon May 24 10:34:38 2010 antoine luciani
+** Last update Mon May 24 10:40:52 2010 antoine luciani
+*/
+
+#ifndef BTREE_H_
+# define BTREE_H_
+
+typedef struct		s_btree_node
+{
+  struct s_btree_node	*left_ptr;
+  struct s_btree_node	*right_ptr;
+  void			*data;
+  void			*sort_data;
+}			t_btree_node;
+
+#endif /* !BTREE_H_ */
