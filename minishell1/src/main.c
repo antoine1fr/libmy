510ef1f3f9aa63c3dda527cbe4c96053414a5d2b
ant01n3 <ant01n3@5f1e6f3c-8dd1-45c4-9ebf-e2a42b03f746>
2010-06-01 10:03:46 +0000

    Creation of the project minishell1.
    git-svn-id: http://svn2.assembla.com/svn/ant01n3/trunk@171 5f1e6f3c-8dd1-45c4-9ebf-e2a42b03f746

---
+++ minishell1/src/main.c
@@ -0,0 +1,16 @@
+/*
+** main.c for minishell1 in /u/all/lucian_b/cu/work/projects/minishell1
+** 
+** Made by antoine luciani
+** Login   <lucian_b@epitech.net>
+** 
+** Started on  Tue Jun  1 12:00:53 2010 antoine luciani
+** Last update Tue Jun  1 12:01:15 2010 antoine luciani
+*/
+
+#include <stdlib.h>
+
+int		main()
+{
+  return (EXIT_SUCCESS);
+}
