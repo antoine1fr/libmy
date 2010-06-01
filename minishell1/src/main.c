e366f7e10d8a0ce7cdcd5906d7596662723e00d5
ant01n3 <ant01n3@5f1e6f3c-8dd1-45c4-9ebf-e2a42b03f746>
2010-06-01 12:30:55 +0000

    In minishell1 : removed libmy for upgrade purpose.
    git-svn-id: http://svn2.assembla.com/svn/ant01n3/trunk@174 5f1e6f3c-8dd1-45c4-9ebf-e2a42b03f746

--- minishell1/src/main.c
+++ minishell1/src/main.c
@@ -5,7 +5,7 @@
 ** Login   <lucian_b@epitech.net>
 ** 
 ** Started on  Tue Jun  1 12:00:53 2010 antoine luciani
-** Last update Tue Jun  1 14:08:58 2010 antoine luciani
+** Last update Tue Jun  1 14:19:23 2010 antoine luciani
 */
 
 #include <stdlib.h>
@@ -14,11 +14,25 @@
 #include <stdio.h>
 #include <string.h>
 
+#include "my.h"
+
 char		*get_path_from_env(const char **env)
 {
+  int		size;
+  int		i;
+
+  i = 0;
+  while (env[i])
+    {
+      size = my_strlen(env[i]);
+      if (size >= 6 && my_strncmp(env[i], "PATH=", 5) == 0)
+	return (env[i]);
+      i += 1;
+    }
+  return (0);
 }
 
-int		main(int argc, char **argv, char **env)
+int		main(int argc, const char **argv, const char **env)
 {
   int		i;
 
