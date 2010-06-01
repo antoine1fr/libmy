50571c9be03019a496dcb1dddd6087bc688266ef
ant01n3 <ant01n3@5f1e6f3c-8dd1-45c4-9ebf-e2a42b03f746>
2010-06-01 16:10:08 +0000

    In libmy : reorganized a bit my.h.
    git-svn-id: http://svn2.assembla.com/svn/ant01n3/trunk@184 5f1e6f3c-8dd1-45c4-9ebf-e2a42b03f746

--- my/include/my.h
+++ my/include/my.h
@@ -5,38 +5,44 @@
 ** Login   <lucian_b@epitech.net>
 ** 
 ** Started on  Fri Mar 19 10:47:57 2010 antoine luciani
-** Last update Tue Jun  1 15:43:29 2010 antoine luciani
+** Last update Tue Jun  1 17:55:32 2010 antoine luciani
 */
 
 #ifndef MY_H
 # define MY_H
 
-# define MY_FALSE 0
-# define MY_TRUE 1
-
-typedef char t_my_bool;
+typedef enum	e_my_bool
+  {
+    MY_FALSE,
+    MY_TRUE
+  }		t_my_bool;
 
 int	my_find_prime_sup(int nb);
-int	my_getnbr(char *str);
-int	my_getnbr_base(char *str, char *base);
 int	my_is_prime(int nbr);
 void	my_isneg(int n);
+
 int	my_power_rec(int nb, int power);
-void	my_put_nbr(int n);
-void	my_puterr(char *str);
-int	my_putnbr_base(int nbr, char *base);
-void	my_putchar(char c);
-void	my_putstr(const char *str);
 char	*my_revstr(char *str);
-void	my_show_to_wordtab(char **tab);
 void	my_showmem(char *str, int size);
 void	my_showstr(char *str);
 void	my_sort_int_tab(int *tab, int size);
 int	my_square_root(int nb);
+
+void	my_swap(int *p1, int *p2);
+void	*my_memset(void *ptr, int value, int size);
+void	*xmalloc(int size);
+void	my_memcpy(void *dest, void *src, unsigned int size);
+
+int	my_getnbr(char *str);
+int	my_getnbr_base(char *str, char *base);
+
+void	my_puterr(char *str);
+void	my_put_nbr(int n);
+int	my_putnbr_base(int nbr, char *base);
+void	my_putchar(char c);
+void	my_putstr(const char *str);
+
 char	*my_strtok(const char *str, char delim);
-char	**my_str_to_wordtab(const char *str);
-char	**my_str_to_wordtab_delim(const char *str, char delim);
-void	my_free_to_wordtab(char **tab);
 char	*my_strcapitalize(char *str);
 char	*my_strcat(char *dest, const char *src);
 int	my_strcmp(const char *s1, const char *s2);
@@ -49,19 +55,20 @@ int	my_strncmp(const char *s1, const char *s2, int n);
 char	*my_strncpy(char *dest, const char *src, int n);
 char	*my_strstr(char *str, char *to_find);
 char	*my_strupcase(char *str);
-void	my_swap(int *p1, int *p2);
-void	*my_memset(void *ptr, int value, int size);
-void	*xmalloc(int size);
 char	*my_strdup(char *str);
 char	*my_strndup(char *src, int n);
-char	*my_untostr(unsigned int n, char *base);
-char	*my_ntostr(int n, char *base);
-void	my_memcpy(void *dest, void *src, unsigned int size);
 
 t_my_bool	my_str_isalpha(char *str);
 t_my_bool	my_str_islower(const char *str);
 t_my_bool	my_str_isnum(const char *str);
 t_my_bool	my_str_isprintable(const char *str);
 t_my_bool	my_str_isupper(const char *str);
+
+char	**my_str_to_wordtab(const char *str);
+char	**my_str_to_wordtab_delim(const char *str, char delim);
+void	my_show_to_wordtab(char **tab);
+void	my_free_to_wordtab(char **tab);
+char	*my_untostr(unsigned int n, char *base);
+char	*my_ntostr(int n, char *base);
 
 #endif /* !MY_H */
