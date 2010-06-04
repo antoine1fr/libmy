1e42aa2d02a610e040ed5791220420226e0680b3
ant01n3 <ant01n3@5f1e6f3c-8dd1-45c4-9ebf-e2a42b03f746>
2010-06-04 17:26:54 +0000

    Creation of wolf3d project.
    git-svn-id: http://svn2.assembla.com/svn/ant01n3/trunk@206 5f1e6f3c-8dd1-45c4-9ebf-e2a42b03f746

---
+++ wolf3d/src/main.c
@@ -0,0 +1,16 @@
+/*
+** main.c for wolf3d in /u/all/lucian_b/cu/work/projects/wolf3d
+** 
+** Made by antoine luciani
+** Login   <lucian_b@epitech.net>
+** 
+** Started on  Fri Jun  4 19:25:10 2010 antoine luciani
+** Last update Fri Jun  4 19:25:32 2010 antoine luciani
+*/
+
+#include <stdlib.h>
+
+int		main()
+{
+  return (EXIT_SUCCESS);
+}
